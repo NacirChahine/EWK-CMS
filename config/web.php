e2bdb3db0e02f40cb722b45263b7ae7e52336a12
Nacir <nacir.chahine@ismena.com>
2024-01-24 10:08:10 +0200

    enable prettyUrl

--- config/web.php
+++ config/web.php
@@ -42,14 +42,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
