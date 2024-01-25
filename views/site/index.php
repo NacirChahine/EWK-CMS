a7b42bf3cfdb990c60eb02c7520c55e6967a962d
Nacir <nacir.chahine@ismena.com>
2024-01-25 15:01:40 +0200

    some stats on site index page

--- views/site/index.php
+++ views/site/index.php
@@ -1,9 +1,18 @@
 <?php
 
 use app\models\Clients;
+use app\models\Services;
+use app\models\Orders;
 
 $this->title = Yii::t('app', 'Dashboard');
 $this->params['breadcrumbs'] = [['label' => $this->title]];
+
+// Fetch counts from the database
+$clientCount = Clients::find()->count();
+$serviceCount = Services::find()->count();
+$openOrderCount = Orders::find()->count();
+$totalOrderCount = Orders::find()->count();
+
 ?>
 <div class="container-fluid">
     <div class="row">
@@ -12,7 +21,6 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
             <h3><?= Yii::t('app', 'Welcome to EWK CMS') ?></h3> <br/>
             <h5><?= Yii::t('app', 'Below some fast stats:') ?></h5>
 
-
         </div>
     </div>
     <div class="row">
@@ -20,8 +28,8 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
 
             <div class="small-box bg-info">
                 <div class="inner">
-                    <h3>33</h3>
-                    <p><?= Yii::t('app', 'Registered Client') ?></p>
+                    <h3><?= $clientCount ?></h3>
+                    <p><?= Yii::t('app', 'Registered Clients') ?></p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
@@ -33,7 +41,7 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
 
             <div class="small-box bg-warning">
                 <div class="inner">
-                    <h3><?= 33 ?></h3>
+                    <h3><?= $serviceCount ?></h3>
                     <p><?= Yii::t('app', 'Services') ?></p>
                 </div>
                 <div class="icon">
@@ -46,7 +54,7 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
 
             <div class="small-box bg-danger">
                 <div class="inner">
-                    <h3><?= 33 ?></h3>
+                    <h3><?= $openOrderCount ?></h3>
                     <p><?= Yii::t('app', 'Open Orders') ?></p>
                 </div>
                 <div class="icon">
@@ -58,7 +66,7 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
 
             <div class="small-box bg-success">
                 <div class="inner">
-                    <h3><?= 33 ?></h3>
+                    <h3><?= $totalOrderCount ?></h3>
                     <p><?= Yii::t('app', 'Total Orders') ?></p>
                 </div>
                 <div class="icon">
